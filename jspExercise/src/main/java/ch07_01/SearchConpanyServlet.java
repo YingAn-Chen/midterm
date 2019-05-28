e2ff7cbff5c83315f09c357e82d0f637e8aa4995
陳膺安 <larry8028@gmail.com>
2019-05-28 19:35:53 +0800

    第二次上傳

--- jspExercise/src/main/java/ch07_01/SearchConpanyServlet.java
+++ jspExercise/src/main/java/ch07_01/SearchConpanyServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/ch07_01/Search.do")
-public class SearchHighwayServlet extends HttpServlet {
+public class SearchConpanyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
